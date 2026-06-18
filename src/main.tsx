1d3d863d758e8d3945b5c94c18bbcba5df8647ce
twldnjs <jwn032@gmail.com>
2026-06-19 00:09:53 +0900

    style: Prettier 세미콜론 적용 (semi: true)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Root element #root not found')
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element #root not found');
 
 createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
